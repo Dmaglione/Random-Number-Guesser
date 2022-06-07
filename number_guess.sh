a7da97ddac4a41a30fc513be3c44ff7c44a76c9f
Dmaglione <60826980+Dmaglione@users.noreply.github.com>
2022-06-07 16:20:24 -0400

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -3,7 +3,8 @@ GAME(){
   #game function generate number "Guess the secret number between 1 and 1000:"
   GUESSCOUNT=0
   MYSTERY=$((RANDOM % 1000 + 1))
-  echo "CHEAT FOR TESTING: $MYSTERY"
+  #echo "CHEAT FOR TESTING: $MYSTERY"
+  #echo statement used for bug testing
   echo "Guess the secret number between 1 and 1000:"
   read GUESS
   CHECK $GUESS
@@ -13,11 +14,13 @@ CHECK(){
   #start function with incrementating game count
 
   if [[ ! $1 =~ ^[0-9]+$ ]]
+  #checking that given argument is valid input, re-run guess and check function if not
   then
   echo "That is not an integer, guess again:"
   read GUESS
   CHECK $GUESS
   else
+  #given valid number compare against mystery number, if incorrect re-run the guess and check function while incrementing the guess count integer
   if [[ $1 = $MYSTERY ]]
     then
       echo "You guessed it in $GUESSCOUNT tries. The secret number was $MYSTERY. Nice job!"
@@ -37,13 +40,15 @@ CHECK(){
   fi
 fi
 }
-
+#Function used to announce win and update databases with relevant info.
 WIN(){
  
   GAME_COUNT=$($PSQL "SELECT games_played from users WHERE name='$NAME'")
   GAME_COUNT=$((GAME_COUNT+1))
   GAME_COUNT_UPDATE=$($PSQL "UPDATE users SET games_played=$GAME_COUNT WHERE name='$NAME'")
   BEST_GUESS=$($APSQL "SELECT best_game FROM users WHERE name='$NAME'")
+  #series of queries to update total played game count and check the best score of the player
+  #if the passed number of guesscount is less than the best guess record, the new record is updated in the database
   if [[ $1 -lt  $BEST_GUESS ]]
     then
     NEW_RECORD=$($PSQL "UPDATE users SET best_game=$1 WHERE name='$NAME'")
@@ -51,6 +56,9 @@ WIN(){
 }
 APSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 PSQL="psql --username=freecodecamp --dbname=number_guess -t -c"
+
+#Seperate PSQL queries for aligned and non-aligned
+#ask for username, if they exist in the database, retrieve their info and greet, otherwise run generic greeting. 
 echo "Enter your username:"
 read NAME
 WELCOME=$($PSQL "SELECT name, games_played, best_game from users WHERE name='$NAME'")
@@ -66,6 +74,7 @@ if [[ -z $WELCOME ]]
  do
     echo -e "Welcome back, $NEW_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   done
+  #Function to initiate the game after introductions
   GAME
 
 fi
